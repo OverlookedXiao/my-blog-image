9a166396a9da1ab91ec1e72181a1c0c1fc0ed67e
OverlookedXiao <1520559187@qq.com>
2023-06-05 15:32:34 +0800

    update my blog img

--- my-blog-img-update.bat
+++ my-blog-img-update.bat
@@ -1,6 +1,6 @@
 call git add .
 call git commit -m "update my blog img"
-rem call git config http.proxy http://127.0.0.1:12345
-rem call git config https.proxy https://127.0.0.1:12345
+call git config http.proxy http://127.0.0.1:50356
+call git config https.proxy https://127.0.0.1:50356
 call git push
 pause
